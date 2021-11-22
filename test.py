225492d142da47e2ac54d53bdb74dd720d62b48a
chandrahassai77 <58036219+chandrahassai77@users.noreply.github.com>
2021-11-22 22:00:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,13 +5,43 @@ import unittest
 from ts import is_even
 
 class TestEven(unittest.TestCase):
-    def test_list_int(self):
+    def test_1(self):
         """
         Test case to check if given input is even
         """
         data = 23
         result = is_even(data)
         self.assertEqual(result, False)
+    def test_2(self):
+        """
+        Test case to check if given input is even
+        """
+        data = 76
+        result = is_even(data)
+        self.assertEqual(result, True)
+    def test_3(self):
+        """
+        Test case to check if given input is even
+        """
+        data = 1257
+        result = is_even(data)
+        self.assertEqual(result, False)
+    def test_4(self):
+        """
+        Test case to check if given input is even
+        """
+        data = 43
+        result = is_even(data)
+        self.assertEqual(result, False)
+    def test_5(self):
+        """
+        Test case to check if given input is even
+        """
+        data = 392
+        result = is_even(data)
+        self.assertEqual(result, True)
+
+
 
 if __name__ == '__main__':
     unittest.main()
